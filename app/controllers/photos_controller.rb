585f84b7a6009ba5d8b6ac364884ba1ac3b602ee
aaronjfinley <aaronjfinley@gmail.com>
2015-07-28 08:06:36 -0500

    photogram competed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,38 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_id = params["id"]
+    photos_db = Photo.all
+    @photo_url = photos_db.find(@photo_id).source
+    @photo_caption = photos_db.find(@photo_id).caption
+
+    render("photo_details.html.erb")
+  end
+
+  def destroy
+
+    photo_id = params["id"]
+    row = Photo.find(photo_id)
+    row.destroy
+
+    redirect_to('/photos')
+    # @list_of_photos = Photo.all
+    # render("index.html.erb")
+
+  end
+
+  def edit
+
+    redirect_to('/photos')
+
+  end
+
+  def new_form
+
+    render("new_form.html.erb")
+
+  end
+
 
 end
